0ca7a2c453eaa664377ec98dcdb1e626223f98f0
Simon Wakenhut <simon.wakenhut@telekom.de>
2020-10-31 15:29:12 +0100

    Fix enabled services

--- controller/services.js
+++ controller/services.js
@@ -8,49 +8,49 @@ var router = express.Router()
 router.get('/', keycloak.enforcer({response_mode: 'token'}), async (req, res) => {
   var services = [
     {
-      'enabled': true,
+      'enabled': config.enabled.gitlab,
       'name': 'GitLab',
       'logo': '/gitlab-logo.png',
       'description': 'From project planning and source code management to CI/CD and monitoring, GitLab is a complete DevOps platform, delivered as a single application.',
       'link': 'https://' + config.domains.gitlab
     },
     {
-      'enabled': true,
+      'enabled': config.enabled.gitea,
       'name': 'Gitea',
       'logo': '/gitea-logo.png',
       'description': 'Gitea is a community managed lightweight code hosting solution written in Go. It is published under the MIT license.',
       'link': 'https://' + config.domains.gitea
     },
     {
-      enabled: true,
+      enabled: config.enabled.jenkins,
       'name': 'Jenkins',
       'logo': '/jenkins-logo.png',
       'description': 'The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project.',
       'link': 'https://' + config.domains.jenkins
     },
     {
-      'enabled': true,
+      'enabled': config.enabled.mattermost,
       'name': 'Mattermost',
       'logo': '/mattermost-logo.png',
       'description': 'Mattermost is a flexible, open source messaging platform that enables secure team collaboration.',
       'link': 'https://' + config.domains.mattermost
     },
     {
-      'enabled': true,
+      'enabled': config.enabled.nexus,
       'name': 'Nexus',
       'logo': '/nexus-logo.png',
       'description': 'The free artifact repository with universal format support.',
       'link': 'https://' + config.domains.nexus
     },
     {
-      'enabled': false,
+      'enabled': config.enabled.openproject,
       'name': 'OpenProject',
       'logo': '/openproject-logo.png',
       'description': 'OpenProject is a web-based project management system for location-independent team collaboration.',
       'link': 'https://' + config.domains.openproject
     },
     {
-      'enabled': true,
+      'enabled': config.enabled.keycloak,
       'name': 'Keycloak',
       'logo': '/keycloak-logo.png',
       'description': 'Keycloak is an open source Identity and Access Management solution aimed at modern applications and services.',
